c7dc643932893a959d34c831d1a13811556943fc
M.R.Z <zgd1348833@gmail.com>
2018-01-02 21:10:57 +0100

    [update] update

--- TweetScraper/items.py
+++ TweetScraper/items.py
@@ -4,21 +4,13 @@
 from scrapy import Item, Field
 
 
-class Tweet(Item):
+class TweetItem(Item):
     ID = Field()       # tweet id
     url = Field()      # tweet url
     datetime = Field() # post time
     text = Field()     # text content
     user_id = Field()  # user id
-    usernameTweet = Field() # username of tweet
-
-    nbr_retweet = Field()  # nbr of retweet
-    nbr_favorite = Field() # nbr of favorite
-    nbr_reply = Field()    # nbr of reply
-
-    is_reply = Field()   # boolean if the tweet is a reply or not
-    is_retweet = Field() # boolean if the tweet is just a retweet of another tweet
-
+    
     has_image = Field() # True/False, whether a tweet contains images
     images = Field()    # a list of image urls, empty if none
 
@@ -29,8 +21,9 @@ class Tweet(Item):
     medias = Field()    # a list of media
 
 
-class User(Item):
+class UserItem(Item):
     ID = Field()            # user id
     name = Field()          # user name
     screen_name = Field()   # user screen name
     avatar = Field()        # avator url
+    
